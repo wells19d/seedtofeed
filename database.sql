57c129c5f9f85daafbd3c7316a023bc1988ff34c
Steve Burket <burketsteve@gmail.com>
2021-07-02 17:04:04 -0500

    updated tables

--- database.sql
+++ database.sql
@@ -8,23 +8,26 @@
 
 CREATE TABLE "user" (
 	"id" serial NOT NULL,
-	"username" varchar(80) UNIQUE NOT NULL, -- frontend display as email
-	"password" varchar(1000) NOT NULL,
-	"farmer" BOOLEAN NOT NULL,
-	"buyer" BOOLEAN NOT NULL,
+	"username" varchar(60) NOT NULL UNIQUE,
+	"password" varchar(60) NOT NULL,
+	"farmer" BOOLEAN(255) NOT NULL,
+	"buyer" BOOLEAN(255) NOT NULL,
 	"first_name" varchar(255) NOT NULL,
 	"last_name" varchar(255) NOT NULL,
-	"super_admin" BOOLEAN NOT NULL,
+	"super_admin" BOOLEAN(255) NOT NULL,
 	CONSTRAINT "user_pk" PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
+
+
+
 CREATE TABLE "field" (
 	"id" serial NOT NULL,
 	"year" DATE,
-	"location" TEXT,
-	"acres" FLOAT,
-	"field_note" TEXT,
+	"location" TEXT(255),
+	"acres" FLOAT(255),
+	"field_note" TEXT(2000),
 	"name" varchar(255),
 	"image" VARCHAR(255),
 	"shape_file" TEXT,
@@ -34,6 +37,9 @@ CREATE TABLE "field" (
 ) WITH (
   OIDS=FALSE
 );
+
+
+
 CREATE TABLE "crop" (
 	"id" serial NOT NULL,
 	"crop_type" varchar(255) NOT NULL,
@@ -41,18 +47,22 @@ CREATE TABLE "crop" (
 ) WITH (
   OIDS=FALSE
 );
+
+
+
 CREATE TABLE "transaction_type" (
 	"id" integer NOT NULL,
-	"name" varchar(255) NOT NULL,
-	"crop_id" integer NOT NULL,
-	"order" integer NOT NULL,
+	"name" varchar(255) NOT NULL
 	CONSTRAINT "transaction_type_pk" PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
+
+
+
 CREATE TABLE "contract" (
 	"id" serial NOT NULL,
-	"user_id" integer NOT NULL,
+	"user_field_id" integer NOT NULL,
 	"commodity" integer NOT NULL,
 	"open_status" integer NOT NULL,
 	"bushel_uid" integer NOT NULL UNIQUE,
@@ -68,31 +78,40 @@ CREATE TABLE "contract" (
 ) WITH (
   OIDS=FALSE
 );
+
+
+
 CREATE TABLE "NIR" (
 	"id" serial NOT NULL,
 	"field_id" integer NOT NULL,
 	"oil" FLOAT,
 	"moisture" FLOAT,
 	"protein" FLOAT,
-    "energy" FLOAT,
-    "amino_acids" FLOAT,
+	"energy" FLOAT,
+	"amino_acids" FLOAT NOT NULL,
 	"tested_at" TIMESTAMP NOT NULL,
 	CONSTRAINT "NIR_pk" PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
+
+
+
 CREATE TABLE "field_transactions" (
 	"id" serial NOT NULL,
 	"field_id" integer NOT NULL,
 	"timestamp" TIMESTAMP NOT NULL,
 	"status_notes" TEXT NOT NULL,
 	"image" varchar(255) NOT NULL,
-	"field_status" TEXT NOT NULL,
+	"field_status" TEXT(255) NOT NULL,
 	"transaction_type" integer NOT NULL,
 	CONSTRAINT "field_transactions_pk" PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
+
+
+
 CREATE TABLE "user_field" (
 	"id" serial NOT NULL,
 	"field_id" integer NOT NULL,
@@ -101,6 +120,9 @@ CREATE TABLE "user_field" (
 ) WITH (
   OIDS=FALSE
 );
+
+
+
 CREATE TABLE "arbitrary_data" (
 	"id" serial NOT NULL,
 	"key" varchar(255),
@@ -110,25 +132,40 @@ CREATE TABLE "arbitrary_data" (
 ) WITH (
   OIDS=FALSE
 );
+
+
+
 CREATE TABLE "contract_status" (
 	"id" serial NOT NULL,
-	"name" varchar(255) NOT NULL,
-	"order" integer NOT NULL,
+	"name" varchar(255) NOT NULL
 	CONSTRAINT "contract_status_pk" PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
+
+
+
+
 ALTER TABLE "field" ADD CONSTRAINT "field_fk0" FOREIGN KEY ("crop_id") REFERENCES "crop"("id");
+
+
 ALTER TABLE "transaction_type" ADD CONSTRAINT "transaction_type_fk0" FOREIGN KEY ("crop_id") REFERENCES "crop"("id");
-ALTER TABLE "contract" ADD CONSTRAINT "contract_fk0" FOREIGN KEY ("user_id") REFERENCES "user"("id");
+
+ALTER TABLE "contract" ADD CONSTRAINT "contract_fk0" FOREIGN KEY ("user_field_id") REFERENCES "user_field"("id");
 ALTER TABLE "contract" ADD CONSTRAINT "contract_fk1" FOREIGN KEY ("commodity") REFERENCES "crop"("id");
 ALTER TABLE "contract" ADD CONSTRAINT "contract_fk2" FOREIGN KEY ("open_status") REFERENCES "contract_status"("id");
+
 ALTER TABLE "NIR" ADD CONSTRAINT "NIR_fk0" FOREIGN KEY ("field_id") REFERENCES "field"("id");
+
 ALTER TABLE "field_transactions" ADD CONSTRAINT "field_transactions_fk0" FOREIGN KEY ("field_id") REFERENCES "field"("id");
 ALTER TABLE "field_transactions" ADD CONSTRAINT "field_transactions_fk1" FOREIGN KEY ("transaction_type") REFERENCES "transaction_type"("id");
+
 ALTER TABLE "user_field" ADD CONSTRAINT "user_field_fk0" FOREIGN KEY ("field_id") REFERENCES "field"("id");
 ALTER TABLE "user_field" ADD CONSTRAINT "user_field_fk1" FOREIGN KEY ("user_id") REFERENCES "user"("id");
+
 ALTER TABLE "arbitrary_data" ADD CONSTRAINT "arbitrary_data_fk0" FOREIGN KEY ("field_trans_id") REFERENCES "field_transactions"("id");
+
+
 
 
 -- Crop Drop Setup
@@ -157,10 +194,10 @@ SELECT * FROM "field"
 JOIN "crop" ON ("crop"."id"="field"."crop_id");
 
 
-INSERT INTO "NIR" (field_id, oil, moisture, protein, tested_at) VALUES ('1', '20', '10', '50', '07-02-2021');
+INSERT INTO "NIR" (field_id, oil, moisture, protein, energy, amino_acids, tested_at) VALUES ('1', '20', '10', '50','60', '200', '07-02-2021');
 
-INSERT INTO "contract" ("user_id", "commodity", "open_status", "bushel_uid", "quantity_fulfilled", "price", "protein", "oil", "moisture", "contract_quantity", "container_serial", "contract_handler")
-VALUES (1, 4, 1, 222, 100, 1, .3, .5, .6, .7, 200, 'Bushel');
+INSERT INTO "contract" ("user_field_id", "commodity", "open_status", "bushel_uid", "quantity_fulfilled", "price", "protein", "oil", "moisture", "contract_quantity", "container_serial", "contract_handler")
+VALUES (1, 4, 2, 222, 100, 1, .3, .5, .6, .7, 200, 'Bushel');
 
 INSERT INTO "transaction_type" ("id", "name") VALUES ('1', 'pre-planting');
 INSERT INTO "transaction_type" ("id", "name") VALUES ('2', 'plant');
@@ -170,3 +207,27 @@ INSERT INTO "transaction_type" ("id", "name") VALUES ('5', 'elevator');
 INSERT INTO "transaction_type" ("id", "name") VALUES ('6', 'processing');
 INSERT INTO "transaction_type" ("id", "name") VALUES ('7', 'transit');
 
+-- not sure if this is doing what it should be doing
+-- SELECT * FROM "contract"
+-- JOIN "user" ON ("user"."id"="contract"."user_field_id");
+
+
+-- This would get us the user information AND the field information on the contract
+-- Tying contract to field
+-- This gets all farmers for the field
+-- This works IF the contract table uses "field_id"
+
+-- before fix
+-- SELECT * FROM "contract"
+-- JOIN "field" ON ("field"."id"="contract"."field_id")
+-- JOIN "user_field" ON ("user_field"."field_id"="field"."id")
+-- JOIN "user" ON ("user"."id"="user_field"."user_id")
+-- WHERE "user"."farmer"=true;
+
+-- Tying contract to user_field
+-- This gets only a single farmer
+-- This works IF the contract table uses "user_field_id"
+SELECT * FROM "contract"
+JOIN "user_field" ON ("user_field"."id"="contract"."user_field_id")
+JOIN "user" ON ("user"."id"="user_field"."user_id")
+JOIN "field" ON ("field"."id"="user_field"."field_id");
