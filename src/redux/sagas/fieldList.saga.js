df624744f56ce54a447ceeab0ae35f1c94437bf7
Tanya Engelhart <technicaltanya@gmail.com>
2021-07-08 10:48:14 -0500

    updated save

--- src/redux/sagas/fieldList.saga.js
+++ src/redux/sagas/fieldList.saga.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 function* fieldList(action) {
     try {
         const response = yield axios.get(`/api/field/fieldList/${action.payload}`)
-            // console.log('The fieldList db response:', response);
+        // console.log('The fieldList db response:', response);
         yield put({ type: 'SET_FIELD_LIST', payload: response.data })
     } catch (error) {
         console.log('User get request failed', error);
