d25a5f30c6993623b4ae18c4f83a2698e687f72b
Steve Burket <burketsteve@gmail.com>
2021-07-09 16:49:52 -0500

    deleted userID from sagas

--- src/redux/sagas/deleteTransaction.saga.js
+++ src/redux/sagas/deleteTransaction.saga.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 function* deleteTransaction(action) {
     try {
         const response = yield axios.delete(`/api/field/delete_transaction/${action.payload}`) // This route does not yet exist.
-        yield put({ type: 'FETCH_FIELD_TRANSACTIONS', payload: INSERT }) // Change INSERT to user ID. Cannot remember how to do so at the moment.
+        yield put({ type: 'FETCH_FIELD_TRANSACTIONS', payload: INSERT }) // do we need a payload for a delete?
     } catch (error) {
         console.log('User get request failed', error);
     }
