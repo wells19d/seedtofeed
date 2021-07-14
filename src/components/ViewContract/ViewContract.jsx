408e1ca49577ccb3db2c83a1051e42922ed73bac
AJ Wells <ajwells@ajs-macbook-pro.lan>
2021-07-14 15:26:11 -0500

    contracts view

--- src/components/ViewContract/ViewContract.jsx
+++ src/components/ViewContract/ViewContract.jsx
@@ -2,55 +2,137 @@ import React, { useEffect } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+
 function ViewContract() {
+  const history = useHistory();
+  const params = useParams();
+  const dispatch = useDispatch();
 
-    const history = useHistory();
-    const params = useParams();
-    const dispatch = useDispatch();
+  const contract = useSelector((store) => store.contractListReducer); // Currently we only have contract list in server and planned reducer. Must create a specific view now.
+  console.log('here is the contract list:', contract);
 
-    const contract = useSelector(store => store.contractListReducer); // Currently we only have contract list in server and planned reducer. Must create a specific view now.
-    console.log('here is the contract list:', contract);
+  const contractID = params.contractID;
+  console.log('here is the contract ID:', contractID);
 
-    const contractID = params.contractID;
-    console.log('here is the contract ID:', contractID);
+  useEffect(() => {
+    dispatch({
+      type: 'FETCH_CONTRACT_LIST',
+      payload: contractID,
+    });
+  }, []);
 
-    useEffect(() => {
-        dispatch({
-            type: 'FETCH_CONTRACT_LIST',
-            payload: contractID
-        })
-    }, [])
-
-    const foundContract = contract.find((contract) => {
-        console.log(contract);
-        return contract.contractID === Number(params.contractID);
+  const foundContract = contract.find((contract) => {
+    console.log(contract);
+    return contract.contractID === Number(params.contractID);
   });
 
   console.log('here is the current contract:', foundContract);
 
-    return (
-        <>
-            <div>
-                <h3>Contract Details</h3>
-                <b>{foundContract?.contract_handler}</b>
+  return (
+    <center>
+      <h3>Contract Details</h3>
+      
+      <Grid container spacing={3}>
+        <Grid item xs={4} />
 
-                <p><span>Contract ID:</span>   <span>{foundContract?.contractID}</span></p> 
-                <p><span>Grower:</span>   <span>{foundContract?.first_name} {foundContract?.last_name}</span></p>
-                <p><span>Commodity:</span>   <span>{foundContract?.crop_type}</span></p>
-                <p><span>Status:</span>   <span>{foundContract?.name}</span></p>
-                <p><span>Contract Quantity:</span>   <span>{foundContract?.contract_quantity}</span></p>
-                <p><span>Quantity Fulfilled:</span>   <span>{foundContract?.quantity_fulfilled}</span></p>
-                <p><span>Container S/N:</span>   <span>{foundContract?.container_serial}</span></p>
-                <p><span>Price:</span>   <span>{foundContract?.price}</span></p>
+        <Grid item>
+        <h4>{foundContract?.contract_handler}</h4>
+        <TableContainer component={Paper}>
+            <Table size="small">
+                <TableHead>
+                    <TableRow>
+                        <TableCell align="right">
+                            <br />
+                            Contract ID:<br/>
+                            Grower:<br />
+                            Commodity:<br />
+                            Status:<br />
+                            Contract Quantity:<br />
+                            Quantity Fulfilled:<br />
+                            Container S/N:<br />
+                            Price:<br />
+                            <br />
+                        </TableCell>
+                        <TableCell>
+                            <br />
+                            {foundContract?.contractID}<br />
+                            {foundContract?.first_name} {foundContract?.last_name}<br />
+                            {foundContract?.crop_type}<br />
+                            {foundContract?.name}<br />
+                            {foundContract?.contract_quantity}<br />
+                            {foundContract?.quantity_fulfilled}<br />
+                            {foundContract?.container_serial}<br />
+                            {foundContract?.price}<br />
+                            <br />
+                        </TableCell>
+                    </TableRow>
+                </TableHead>
+            </Table>
+        </TableContainer>
+        </Grid>
+        <Grid item>
+        <h4>NIR Analysis:</h4>
+        <TableContainer component={Paper}>
+            <Table size="small">
+                <TableHead>
+                <TableRow>
+                        <TableCell align="right">
+                        <br />
+                        Amino Acid:<br />
+                        Energy:<br />
+                        Protein:<br />
+                        Oil:<br />
+                        Moisture:<br />
+                            <br />
+                            <br />
+                            <br />
+                            <br />
+                        </TableCell>
+                        <TableCell>
+                        <br />
+                        {foundContract?.amino_acids}<br />
+                        {foundContract?.energy}<br />
+                        {foundContract?.protein}<br />
+                        {foundContract?.oil}<br />
+                        {foundContract?.moisture}<br />
+                            <br />
+                            <br />
+                            <br />
+                            <br />
+                        </TableCell>
+                    </TableRow>
+                </TableHead>
+            </Table>
+        </TableContainer>
+        </Grid>
+        <Grid item xs={4} />
+      </Grid>
+    </center>
+  );
+}
 
+export default ViewContract;
+
+/* 
+
+<div>
                 <b><u>NIR Analysis:</u></b>
 
-                <p><span>Amino Acid:</span>   <span>{foundContract?.amino_acids}</span></p> {/* This is living in the NIR table */}
-                <p><span>Energy:</span>   <span>{foundContract?.energy}</span></p> {/* This is living in the NIR table */}
+                <p><span>Amino Acid:</span>   <span>{foundContract?.amino_acids}</span></p>
+                <p><span>Energy:</span>   <span>{foundContract?.energy}</span></p>
                 <p><span>Protein:</span>   <span>{foundContract?.protein}</span></p>
                 <p><span>Oil:</span>   <span>{foundContract?.oil}</span></p>
                 <p><span>Moisture:</span>   <span>{foundContract?.moisture}</span></p>
-          
             </div>
 
             <button onClick={() => history.push(`/field_details/${foundContract.fieldID}`)}>
@@ -59,8 +141,5 @@ function ViewContract() {
             <button onClick={() => history.push(`/edit_contract/${foundContract.contractID}`)}>
                 Edit Contract
             </button>
-        </>
-    )
-}
 
-export default ViewContract;
+*/
