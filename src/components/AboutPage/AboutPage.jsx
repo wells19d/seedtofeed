784786312dda183d47c2d886708dfb9623c53318
Tanya Engelhart <technicaltanya@gmail.com>
2021-07-19 15:06:47 -0500

    updated about page to slider

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -15,6 +15,10 @@ import photo8 from './images/8.png';
 import Button from '@material-ui/core/Button';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+import ImageList from '@material-ui/core/ImageList';
+import ImageListItem from '@material-ui/core/ImageListItem';
+import ImageListItemBar from '@material-ui/core/ImageListItemBar';
+import IconButton from '@material-ui/core/IconButton';
 
 // This is one of our simplest components
 // It doesn't have local state,
@@ -27,12 +31,32 @@ function AboutPage() {
 
   const useStyles = makeStyles((theme) => ({
     root: {
-      flexGrow: 1,
+      display: 'flex',
+      flexWrap: 'wrap',
+      justifyContent: 'space-around',
+      overflow: 'hidden',
+      backgroundColor: theme.palette.background.paper,
     },
     paper: {
       padding: theme.spacing(1), //grid padding
       textAlign: 'center',
       color: theme.palette.text.secondary,
+    },
+    imageList: {
+      flexWrap: 'nowrap',
+      // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
+      transform: 'translateZ(0)',
+      color: theme.palette.text.secondary,
+
+    },
+    title: {
+      color: 'black',
+    },
+    titleBar: {
+      background:
+        'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+      color: theme.palette.text.default,
+
     },
   }));
   const classes = useStyles();
@@ -55,53 +79,110 @@ function AboutPage() {
               purchasing Seed to Feed product.
             </Paper>
           </Grid>
-
-          <Grid container spacing={0}>
-            <Grid item xs>
-              <Paper className={classes.paper}>
-                <img src={photo1} alt="Planting" className="about-image" width="500"></img>
-              </Paper>
-            </Grid>
-            <Grid item xs>
-              <Paper className={classes.paper}>
-                <img src={photo2} alt="Soybean lifecycle" className="about-image" width="500"></img>
-              </Paper>
-            </Grid>
-            <Grid item xs>
-              <Paper className={classes.paper}>
-                <img src={photo3} alt="Harvest" className="about-image" width="500"></img>
-              </Paper>
-            </Grid>
-            <Grid item xs>
-              <Paper className={classes.paper}>
-                <img src={photo4} alt="Transit to elevator" className="about-image" width="500"></img>
-              </Paper>
-            </Grid>
-            <Grid container spacing={0}>
-
-              <Grid item xs>
-                <Paper className={classes.paper}>
-                  <img src={photo5} alt="Train to storage" className="about-image" width="500"></img>
-                </Paper>
-              </Grid>
-              <Grid item xs>
-                <Paper className={classes.paper}>
-                  <img src={photo6} alt="Shipped around world" className="about-image" width="500"></img>
-                </Paper>
-              </Grid>
-              <Grid item xs>
-                <Paper className={classes.paper}>
-                  <img src={photo7} alt="Mills receive soybeans" className="about-image" width="500"></img>
-                </Paper>
-              </Grid>
-              <Grid item xs>
-                <Paper className={classes.paper}>
-                  <img src={photo8} alt="Mill to farm" className="about-image" width="500"></img>
-                </Paper>
-              </Grid>
-            </Grid >
-          </Grid >
         </Grid>
+
+
+        <ImageList className={classes.imageList} cols={6}>
+          <ImageListItem>
+
+            <img src={photo1} alt="Planting" className="about-image" width="500" />
+            <ImageListItemBar
+              position="bottom"
+              title='Seed'
+              classes={{
+                root: classes.titleBar,
+                title: classes.title,
+              }} />
+          </ImageListItem>
+
+          <ImageListItem>
+            <img src={photo2} alt="Soybean lifecycle" className="about-image" width="500" />
+            <ImageListItemBar
+              position="bottom"
+
+              title='Lifecycle'
+              classes={{
+                root: classes.titleBar,
+                title: classes.title,
+              }} />
+          </ImageListItem>
+
+          <ImageListItem>
+
+            <img src={photo3} alt="Harvest" className="about-image" width="500" />
+            <ImageListItemBar
+              position="bottom"
+
+              title='Harvest'
+              classes={{
+                root: classes.titleBar,
+                title: classes.title,
+              }} />
+          </ImageListItem>
+
+          <ImageListItem>
+
+            <img src={photo4} alt="Transit to elevator" className="about-image" width="500" />
+            <ImageListItemBar
+              position="bottom"
+
+              title='Elevator'
+              classes={{
+                root: classes.titleBar,
+                title: classes.title,
+              }} />
+          </ImageListItem>
+
+          <ImageListItem>
+            <img src={photo5} alt="Train to storage" className="about-image" width="500" />
+            <ImageListItemBar
+              position="bottom"
+
+              title='Storage'
+              classes={{
+                root: classes.titleBar,
+                title: classes.title,
+              }} />
+          </ImageListItem>
+
+          <ImageListItem>
+            <img src={photo6} alt="Shipped around world" className="about-image" width="500" />
+            <ImageListItemBar
+              position="bottom"
+
+              title='Shipped Worldwide'
+              classes={{
+                root: classes.titleBar,
+                title: classes.title,
+              }} />
+          </ImageListItem>
+
+          <ImageListItem>
+            <img src={photo7} alt="Mills receive soybeans" className="about-image" width="500" />
+            <ImageListItemBar
+              position="bottom"
+
+              title='Milling'
+              classes={{
+                root: classes.titleBar,
+                title: classes.title,
+              }} />
+          </ImageListItem>
+
+          <ImageListItem>
+            <img src={photo8} alt="Mill to farm" className="about-image" width="500" />
+            <ImageListItemBar
+              position="bottom"
+
+              title='Feed'
+              classes={{
+                root: classes.titleBar,
+                title: classes.title,
+              }} />
+          </ImageListItem>
+
+
+        </ImageList>
         <br />
         <br />
         <div>
