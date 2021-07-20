60324b4876bd5c0ed07945d6fca0aa9ce6a962c0
AJ Wells <ajwells@ajs-macbook-pro.lan>
2021-07-19 22:29:15 -0500

    landing page

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -3,23 +3,12 @@ import { useHistory } from 'react-router-dom';
 import './LandingPage.css';
 import supplychain from './logos/supplychain.png';
 
-import Button from '@material-ui/core/Button';
+import { Button, Grid } from '@material-ui/core';
 
 // CUSTOM COMPONENTS
 import LoginForm from '../LoginForm/LoginForm';
 
-const submitButton = {
-  border: 'solid black 0px',
-  background: '#fdb41b',
-  padding: '3px 10px',
-  boxShadow: '3px 3px 4px 0px grey',
-};
-
-const standardButtons = {
-  border: 'solid black 0px',
-  boxShadow: '2px 2px 3px 0px grey',
-  minWidth: '1px',
-};
+import '../../index.css';
 
 function LandingPage() {
   const history = useHistory();
@@ -29,28 +18,72 @@ function LandingPage() {
   };
 
   return (
-    <div className="container">
+    <Grid container={1}>
+
+      <Grid item xs={1}/>
+      <Grid itme xs={6}><center>
+      <h1>Welcome to the Seed to Feed Tracker</h1>
+        <img src={supplychain}/>
+              Seed to Feed was created to help increase profitability for
+              farmers and feed producers, as well as the partners in-between.
+              This project will benefit the entire supply chain, all the way
+              back to your local farmer. Our mission is to promote industry
+              education, explore existing supply chain challenges, and highlight
+              technology and industry partners working on solutions.
+            </center>
+            </Grid>
+      <Grid item xs={1}/>
+      <Grid itme xs={3}>
+        <center>
+          <p>
+            <br />
+            <br />
+        <LoginForm />
+          </p>
+      <Button
+              className="submit-buttons"
+              size="small"
+              onClick={registration}
+            >
+              Register User
+            </Button></center></Grid>
+      <Grid item xs={1}/>
+
+    </Grid>
+  );
+}
+
+export default LandingPage;
+
+/*
+
+<div className="container">
       <div className="grid">
         <div className="grid-col grid-col_8">
-          <h1 align="center">Welcome to the Seed to Feed Tracker</h1>
-          <img className="landing-page" src={supplychain} alt="Seed to Feed Logo" />
-          <p>
-            Seed to Feed was created to help increase profitability for farmers
-            and feed producers, as well as the partners in-between. This project
-            will benefit the entire supply chain, all the way back to your local
-            farmer. Our mission is to promote industry education, explore
-            existing supply chain challenges, and highlight technology and
-            industry partners working on solutions.
-          </p>
+          <center>
+            <h1>Welcome to the Seed to Feed Tracker</h1>
+            <img
+              className="landing-page"
+              src={supplychain}
+              alt="Seed to Feed Logo"
+            />
+            <p>
+              Seed to Feed was created to help increase profitability for
+              farmers and feed producers, as well as the partners in-between.
+              This project will benefit the entire supply chain, all the way
+              back to your local farmer. Our mission is to promote industry
+              education, explore existing supply chain challenges, and highlight
+              technology and industry partners working on solutions.
+            </p>
+          </center>
         </div>
         <div className="grid-col grid-col_4">
           <LoginForm />
           <center>
             <br />
             <Button
-              className='submit-buttons'
+              className="submit-buttons"
               size="small"
-              className="btn btn_sizeSm"
               onClick={registration}
             >
               Register User
@@ -59,7 +92,5 @@ function LandingPage() {
         </div>
       </div>
     </div>
-  );
-}
 
-export default LandingPage;
+*/
